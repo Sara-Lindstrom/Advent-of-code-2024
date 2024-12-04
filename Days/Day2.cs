24fbc29a9d9e3e763f97130090c22f76013f1b02
Sara Lindström <111883184+Sara-Lindstrom@users.noreply.github.com>
2024-12-04 17:01:51 +0100

    Update Day2.cs
    removed sessionToken

--- Days/Day2.cs
+++ Days/Day2.cs
@@ -33,7 +33,7 @@ public class Day2
     public async Task Run()
     {
         //HttpRequestSetup
-        string sessionToken = "53616c7465645f5fc8bb81cd507326a704fc03bf2f220a371e76902074c79d7d29c4cf8b1be94115098b6a9f269f3653a8e0839733f85cc423714213f52af5a6";
+        string sessionToken = "";
         string url = $"https://adventofcode.com/2024/day/2/input";
 
         try
